93c5dd49ecd1a145a7fe7a25303e6bd58d84383d
Tonkla Banpluluang <Tonkla Banpluluang@LT-UDVJTST421D0>
2024-09-03 16:02:50 +0700

    git push

--- samplemonogame11/Game1.cs
+++ samplemonogame11/Game1.cs
@@ -44,7 +44,7 @@ namespace samplemonogame11
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            
 
             base.Draw(gameTime);
         }
